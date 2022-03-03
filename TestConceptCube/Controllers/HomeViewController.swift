633e1ff62e2ccebd15fa82e15c90c35e1f52951d
vu <phanvu0313>
2022-03-03 13:41:44 +0700

    first

---
+++ TestConceptCube/Controllers/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestConceptCube
+//
+//  Created by Vũ Phan on 03/03/2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
